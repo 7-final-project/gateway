46a4270726ab4c821104b43e918e0d648298d678
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-03 20:54:57 +0900

    [ refactor ] JWT 인증 필터 리팩토링 ( #5 )
    - 필터의 기능을 관련 메서드별로 그룹화하여 코드 가독성 향상
    - 시간 측정 코드 제거
    - 로그 메시지 및 메서드 주석 추가
    - 메서드 위치 및 이름 변경으로 코드 구조 개선

--- src/main/java/com/qring/gateway/filter/pre/JwtAuthorizationFilterV2.java
+++ src/main/java/com/qring/gateway/filter/pre/JwtAuthorizationFilterV2.java
@@ -3,8 +3,6 @@ package com.qring.gateway.filter.pre;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,72 +24,59 @@ import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j(topic = "JwtAuthorizationFilterV2 Log")
 @ConditionalOnProperty(name = "filter.v2.enabled", havingValue = "true", matchIfMissing = true)
-@Slf4j(topic = "JWT 필터 V2")
 public class JwtAuthorizationFilterV2 implements GlobalFilter, Ordered {
 
-    private final MeterRegistry meterRegistry;
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
 
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
-
-    // Header KEY 값
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-
-    // Token 식별자
-    public static final String BEARER_PREFIX = "Bearer ";
 
     @Value("${service.jwt.secret-key}")
     private String secretKey;
 
+    //---
+    // NOTE: 필터 시작 로직
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String path = exchange.getRequest().getURI().getPath();
 
-        log.info("Path 확인");
         if (path.equals("/v1/auth/login") || path.equals("/v1/users/join")) {
-            log.info("요청 URI가 인증/인가 제외 경로입니다. 필터를 통과합니다.");
+            log.info("인증/인가 제외 경로. 필터를 통과합니다.");
             return chain.filter(exchange);
         }
 
-        log.info("Authorization 헤더에서 JWT 토큰 추출 시도");
         String token = getJwtFromHeader(exchange);
-
         if (token == null || !validateToken(token)) {
-            log.warn("JWT 토큰이 유효하지 않습니다. 요청을 차단합니다.");
+
+            log.warn("유효하지 않은 JWT 토큰입니다. 요청을 차단합니다.");
+
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
 
         String userId = getUserIdFromToken(token);
 
-        log.info("JWT 토큰 검증 성공: Redis 캐시에서 Passport 확인");
-
-        // Timer 시작
-        Timer.Sample sample = Timer.start(meterRegistry);
-
+        log.info("JWT 토큰 검증 성공. Redis에서 Passport 확인");
         return getOrCreatePassport(userId)
                 .flatMap(passportToken -> addPassportToRequest(exchange, passportToken))
-                .flatMap(updatedExchange -> {
-                    sample.stop(Timer.builder("passport.request.time") // Timer 종료
-                            .description("Passport 발급 요청 소요 시간")
-                            .tag("method", "direct")
-                            .register(meterRegistry));
-                    return chain.filter(updatedExchange);
-                });
+                .flatMap(chain::filter);
     }
 
+    //---
+    // NOTE: Authorization 헤더에서 JWT 토큰 추출하는 메서드
     private String getJwtFromHeader(ServerWebExchange exchange) {
         String bearerToken = exchange.getRequest().getHeaders().getFirst(AUTHORIZATION_HEADER);
-
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            log.debug("Authorization 헤더에서 Bearer 토큰 추출 성공");
-            return bearerToken.substring(7);
+            return bearerToken.substring(BEARER_PREFIX.length());
         }
-
         log.warn("Authorization 헤더가 없거나 잘못된 형식입니다.");
         return null;
     }
 
+    // -----
+    // NOTE: JWT 토큰 유효성 검증
     private boolean validateToken(String token) {
         try {
             getClaimsJws(token);
@@ -108,58 +93,56 @@ public class JwtAuthorizationFilterV2 implements GlobalFilter, Ordered {
         return false;
     }
 
+    // -----
+    // NOTE: JWT 토큰에서 Claims 추출
     private Jws<Claims> getClaimsJws(String token) {
         SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token);
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
     }
 
+    // -----
+    // NOTE: JWT 토큰에서 userId 추출
     private String getUserIdFromToken(String token) {
         Jws<Claims> claimsJws = getClaimsJws(token);
         return String.valueOf(claimsJws.getBody().get("userId", Long.class));
     }
 
+    // -----
+    // NOTE: Redis에서 Passport Token을 가져오거나, 없으면 새로 발급하는 메서드
     public Mono<String> getOrCreatePassport(String userId) {
         return reactiveRedisTemplate.opsForValue()
                 .get(userId)
-                .flatMap(existingPassport -> {
-                    log.info("Redis에서 Passport Token 조회 성공: {}", existingPassport);
-                    return Mono.just(existingPassport);
-                })
-                .switchIfEmpty(Mono.defer(() -> {
-                    log.info("Redis에 Passport Token이 없거나 만료되었습니다. 새로 발급 시도");
-                    return requestNewPassport(userId)
-                            .flatMap(newPassport -> reactiveRedisTemplate.opsForValue()
-                                    .set(userId, newPassport, Duration.ofMinutes(5))
-                                    .thenReturn(newPassport));
-                }));
+                .switchIfEmpty(Mono.defer(() -> requestNewPassport(userId)
+                        .flatMap(newPassport -> reactiveRedisTemplate.opsForValue()
+                                .set(userId, newPassport, Duration.ofMinutes(5))
+                                .thenReturn(newPassport))))
+                .doOnSuccess(passport -> log.info("Redis에서 Passport Token 조회 성공: {}", passport));
     }
 
+    // -----
+    // NOTE: 새로운 Passport Token 발급 요청
     private Mono<String> requestNewPassport(String userId) {
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:19005")
-                .build();
-
-        return webClient.post()
+        return WebClient.create("http://localhost:19005")
+                .post()
                 .uri("/v1/auth/passport")
                 .header("X-User-Id", userId)
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(String.class)
+                .doOnError(e -> log.error("Passport 발급 실패: {}", e.getMessage()));
     }
 
-
+    // -----
+    // NOTE: Passport Token을 요청에 추가
     private Mono<ServerWebExchange> addPassportToRequest(ServerWebExchange exchange, String passportToken) {
         ServerHttpRequest updatedRequest = exchange.getRequest()
                 .mutate()
                 .header("X-Passport-Token", passportToken)
                 .build();
-
         return Mono.just(exchange.mutate().request(updatedRequest).build());
     }
 
-
+    // -----
+    // NOTE: 필터의 실행 순서를 결정
     @Override
     public int getOrder() {
         return Ordered.HIGHEST_PRECEDENCE;
