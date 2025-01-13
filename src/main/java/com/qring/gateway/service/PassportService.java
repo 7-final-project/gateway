45fdfb935c70a94e28aceba18d8aa43e22a6b024
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-01-14 07:11:37 +0900

    [ feat ] base-url 설정 추가 ( #11 )

--- src/main/java/com/qring/gateway/service/PassportService.java
+++ src/main/java/com/qring/gateway/service/PassportService.java
@@ -15,6 +15,9 @@ import java.time.Duration;
 @Slf4j(topic = "PassportService")
 public class PassportService {
 
+    @Value("${auth-service.base-url}")
+    private String authServiceBaseUrl;
+
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     // -----
@@ -36,7 +39,7 @@ public class PassportService {
     // -----
     // NOTE: 새로운 Passport Token 발급 요청
     private Mono<String> requestNewPassport(String userId) {
-        return WebClient.create("http://localhost:19005")
+        return WebClient.create(authServiceBaseUrl)
                 .post()
                 .uri("/v1/auth/passport")
                 .header("X-User-Id", userId)
